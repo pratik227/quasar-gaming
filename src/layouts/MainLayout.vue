136dced54481285cdb987cf3ccca7d17ac926454
Pratik Patel <34883558+pratik227@users.noreply.github.com>
2020-10-09 12:27:02 +0530

    Minor changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,19 +2,19 @@
   <q-layout view="lHh Lpr lFf">
 
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
+    import EssentialLink from 'components/EssentialLink.vue'
 
-export default {
-  name: 'MainLayout',
+    export default {
+        name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
-}
+        components: {
+            EssentialLink
+        },
+    }
 </script>
